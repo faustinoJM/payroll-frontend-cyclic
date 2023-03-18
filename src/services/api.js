0d0ec270e04a7f2d06d0544931ea4f2a8a196aeb
Faustino J. Mateus <Faustinojmateus@gmail.com>
2023-03-18 10:48:05 +0200

    services api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'https://nice-pear-sparrow-suit.cyclic.app'
 })
 
 export default api;
+
+  // baseURL: 'http://localhost:3333'
+
+
